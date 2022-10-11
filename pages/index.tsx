9efc63d5dfdebd7e2a109a317091c9f79e1d198a
cremirdevio <josephajibodu@gmail.com>
2022-10-11 20:59:27 +0100

    Hero section responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Heading, HStack, Icon, Image, Link, ListItem, Text, UnorderedList, VStack } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, HStack, Icon, Image, Link, ListItem, Stack, Text, UnorderedList, VStack } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import CallToAction from '../components/CallToAction'
 import Checklist from '../components/Checklist'
@@ -8,17 +8,17 @@ const Home: NextPage = () => {
   return (
     <Box>
       <VStack>
-        <Flex maxW={"800px"} flexDir={"column"} alignItems={'center'} textAlign={'center'} pt="16">
-          <HStack justify={'center'} color="gray.700">
-            <Heading fontSize="5xl" fontWeight={'extrabold'}>Collect.</Heading>
-            <Heading fontSize="5xl" color="orange.500" fontWeight={'extrabold'}>Manage.</Heading>
-            <Heading fontSize="5xl" fontWeight={'extrabold'}>Share.</Heading>
-          </HStack>
+        <Flex maxW={"800px"} flexDir={"column"} alignItems={'center'} textAlign={'center'} pt="16" px='4'>
+          <Stack direction={['column', 'row']} justify={'center'} color="gray.700">
+            <Heading lineHeight='1' fontSize={['7xl', '5xl']} fontWeight={'extrabold'}>Collect.</Heading>
+            <Heading lineHeight='1.2' fontSize={['7xl', '5xl']} bgGradient='linear(to-l, orange.500, green.400)' bgClip='text' fontWeight={'extrabold'}>Manage.</Heading>
+            <Heading lineHeight='1' fontSize={['7xl', '5xl']} fontWeight={'extrabold'}>Share.</Heading>
+          </Stack>
           {/* <Heading color="green.400">Collecting, Managing and Sharing reviews from your clients has been made easier with Reviewgush</Heading> */}
           <Text mt={2} color="gray.500" fontSize={'xl'} fontWeight="light">
             Reviews from satisfied clients/customers can now be collected easily with ReviewGush using video, images, text, ratings, or even a combination of all four formats.
           </Text>
-          <Button as='a' mt={6} rounded={'xl'} minH={12} minW={24} bg="orange.500" color='white' _hover={{ bg: 'orange.600' }} href="#get-started">Start Here</Button>
+          <Button as='a' mt={6} rounded={'xl'} minH={12} minW={32} bg="orange.500" color='white' _hover={{ bg: 'orange.600' }} href="#get-started">Start Here</Button>
         </Flex>
         <Image src='/images/developer.svg' alt='Dan Abramov' width={"600px"} />
       </VStack>
