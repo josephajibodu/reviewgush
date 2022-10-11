e374fef07b5a00dce0937e8140097a099544c96e
cremirdevio <josephajibodu@gmail.com>
2022-10-11 21:14:49 +0100

    Features responsiveness

--- components/NewsletterBox.tsx
+++ components/NewsletterBox.tsx
@@ -3,12 +3,12 @@ import ThreeStrokes from './BackgroundPatterns/ThreeStrokes'
 
 export default function NewsletterBox({...props}: StackProps) {
   return (
-    <Flex overflow='hidden' pos='relative' flexDir='column' align='center' maxWidth='3xl' paddingX='14' paddingY='20' color='white' border='2px' rounded='2xl' textAlign='center' bgColor='green.500' {...props}>
+    <Flex overflow='hidden' pos='relative' flexDir='column' align='center' maxWidth='3xl' paddingX='14' paddingY='20' color='white' rounded='2xl' textAlign='center' bgColor='green.500' {...props}>
       <Heading fontSize='12px' color='white' textTransform='uppercase'>Enjoy 30 days free trial</Heading>
       <Heading fontSize='4xl' color='white'>Sign Up Now</Heading>
       <Text color='white.600'>Be the first to receive the good news from us when we launch. For the first 100 signups, we are giving out 30 days free trial.</Text>
-      <Flex pos='relative' align='center' minWidth='md' maxWidth='lg' marginTop='6'>
-        <Input placeholder='Enter your email' outline='0' minHeight='12' border='2px' borderColor='gray.300' _placeholder={{ color: 'gray.200' }} focusBorderColor='gray.300' />
+      <Flex pos='relative' align='center' minWidth='300px' maxWidth='lg' marginTop='6'>
+        <Input placeholder='Enter your email' outline='0' minHeight='12' pr='32' border='2px' borderColor='gray.300' _placeholder={{ color: 'gray.200' }} focusBorderColor='gray.300' />
         <Button pos='absolute' right='1' bgColor='orange.500' _hover={{ bgColor: 'orange.600' }} zIndex="20">Sign Me Up</Button>
       </Flex>
       <ThreeStrokes pos='absolute' transform='rotate(45deg)' bottom='60%' left='85%' />
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,9 @@ const Home: NextPage = () => {
       <VStack>
         <Flex maxW={"800px"} flexDir={"column"} alignItems={'center'} textAlign={'center'} pt="16" px='4'>
           <Stack direction={['column', 'row']} justify={'center'} color="gray.700">
-            <Heading lineHeight='1' fontSize={['7xl', '5xl']} fontWeight={'extrabold'}>Collect.</Heading>
+            <Heading lineHeight='1.2' fontSize={['7xl', '5xl']} fontWeight={'extrabold'}>Collect.</Heading>
             <Heading lineHeight='1.2' fontSize={['7xl', '5xl']} bgGradient='linear(to-l, orange.500, green.400)' bgClip='text' fontWeight={'extrabold'}>Manage.</Heading>
-            <Heading lineHeight='1' fontSize={['7xl', '5xl']} fontWeight={'extrabold'}>Share.</Heading>
+            <Heading lineHeight='1.2' fontSize={['7xl', '5xl']} fontWeight={'extrabold'}>Share.</Heading>
           </Stack>
           {/* <Heading color="green.400">Collecting, Managing and Sharing reviews from your clients has been made easier with Reviewgush</Heading> */}
           <Text mt={2} color="gray.500" fontSize={'xl'} fontWeight="light">
@@ -23,12 +23,12 @@ const Home: NextPage = () => {
         <Image src='/images/developer.svg' alt='Dan Abramov' width={"600px"} />
       </VStack>
 
-      <HStack px='20' py='8' spacing='6' bgColor='white'>
+      <Stack direction={['column', 'row']} px={[8, 20]} py='8' spacing='6' bgColor='white'>
         <Box>
           <Heading fontSize='sm' color='orange.500' textTransform='uppercase' fontWeight='medium'>Collect</Heading>
-          <Heading color='grey.700' fontSize='5xl' mb='2'>Get feedbacks easily</Heading>
+          <Heading lineHeight='1' color='grey.700' fontSize='5xl' mb='2'>Get feedbacks easily</Heading>
           <Text color='gray.500'>Obtain feedback easily from your satisfied customers/clients about what they like best about your products/services. You can build your collection form in less than a minute.</Text>
-          <UnorderedList color='gray.500' mb='8'>
+          <UnorderedList color='gray.500' mb='8' mt='4'>
             <Checklist as='li'>Less than a minute set-up</Checklist>
             <Checklist as='li'>Collect Text, pictures and videos</Checklist>
             <Checklist as='li'>Easily embed on your website</Checklist>
@@ -38,15 +38,15 @@ const Home: NextPage = () => {
         <Box>
           <Image src='/images/meeting-feature-1.png' alt='Feature 1' />
         </Box>
-      </HStack>
+      </Stack>
 
-      <HStack px='20' py='8' spacing='6' bgColor='white'>
+      <Stack direction={['column', 'row']} px={[8, 20]} py='8' spacing='6' bgColor='white'>
         <Box>
           <Image src='/images/meeting-feature-2.png' alt='Feature 2' />
         </Box>
         <Box>
           <Heading fontSize='sm' color='orange.500' textTransform='uppercase' fontWeight='medium'>Manage</Heading>
-          <Heading color='grey.700' fontSize='5xl' mb='2'>Manage reviews and integrations</Heading>
+          <Heading lineHeight='1'  color='grey.700' fontSize='5xl' mb='2'>Manage reviews and integrations</Heading>
           <Text color='gray.500'>You don{'\''}t have to open an Excel sheet to gather your feedback. ReviewGush gives you a powerful dashboard to manage all your reviews across all platforms.</Text>
           <UnorderedList color='gray.500' mb='8'>
             <Checklist as='li'>You can easily import testimonials from social media and review sites</Checklist>
@@ -54,12 +54,12 @@ const Home: NextPage = () => {
           </UnorderedList>
           <Link href='#' color='green.400'>Learn More</Link>
         </Box>
-      </HStack>
+      </Stack>
 
-      <HStack px='20' py='8' spacing='6' bgColor='white'>
+      <Stack direction={['column', 'row']} px={[8, 20]} py='8' spacing='6' bgColor='white'>
         <Box>
           <Heading fontSize='sm' color='orange.500' textTransform='uppercase' fontWeight='medium'>Share</Heading>
-          <Heading color='grey.700' fontSize='5xl' mb='2'>Share and Display your reviews</Heading>
+          <Heading lineHeight='1' color='grey.700' fontSize='5xl' mb='2'>Share and Display your reviews</Heading>
           <Text color='gray.500'>Rather than sending WhatsApp screenshots of customers{'\''}/clients{'\''} reviews, share the reviews in a more organized format. With ReviewGush, you can share your collected reviews in a variety of formats, including designs.</Text>
           <UnorderedList color='gray.500' mb='8'>
             <Checklist as='li'>Easily shareable in various formats, including pictures</Checklist>
@@ -70,10 +70,10 @@ const Home: NextPage = () => {
         <Box>
           <Image src='/images/meeting-feature-2.png' alt='Feature 2' />
         </Box>
-      </HStack>
+      </Stack>
 
-      <Flex padding='20' justify='center'>
-        <NewsletterBox id='get-started' />
+      <Flex padding={[0, 20]} justify='center'>
+        <NewsletterBox id='get-started' rounded={['none', '2xl']} />
       </Flex>
 
     </Box>
