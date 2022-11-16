b5db817c864d5ac0f18c3213d4ed216d7a06765e
cremirdevio <josephajibodu@gmail.com>
2022-11-16 22:56:10 +0100

    SImple framer motion animation added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
-import { Box, Button, Flex, Heading, HStack, Icon, Image, Link, ListItem, Stack, Text, UnorderedList, VStack } from '@chakra-ui/react'
+import { Box, Button, chakra, Fade, Flex, Heading, HStack, Icon, Image, Link, ListItem, shouldForwardProp, Slide, Stack, Text, UnorderedList, VStack } from '@chakra-ui/react'
+import { isValidMotionProp, motion } from 'framer-motion'
 import type { NextPage } from 'next'
 import CallToAction from '../components/CallToAction'
 import Checklist from '../components/Checklist'
 import NewsletterBox from '../components/NewsletterBox'
 
+const ChakraBox = chakra(motion.div, {
+  shouldForwardProp: (prop) => isValidMotionProp(prop) || shouldForwardProp(prop),
+});
+
 const Home: NextPage = () => {
+
   return (
     <Box>
       <VStack>
@@ -24,7 +30,7 @@ const Home: NextPage = () => {
       </VStack>
 
       <Stack direction={['column', 'row']} px={[8, 20]} py='8' spacing='6' bgColor='white'>
-        <Box width={['100%', '60%']}>
+        <ChakraBox transition={{ duration: 0.5 }} initial={{ translateX: -600, opacity: 0 }} whileInView={{ translateX: 0, opacity: 1 }} width={['100%', '60%']}>
           <Heading fontSize='sm' color='orange.500' textTransform='uppercase' fontWeight='medium'>Collect</Heading>
           <Heading lineHeight='1' color='grey.700' fontSize='5xl' mb='2'>Get feedbacks easily</Heading>
           <Text color='gray.500'>Obtain feedback easily from your satisfied customers/clients about what they like best about your products/services. You can build your collection form in less than a minute.</Text>
@@ -34,17 +40,17 @@ const Home: NextPage = () => {
             <Checklist as='li'>Easily embed on your website</Checklist>
           </UnorderedList>
           <Link href='#' color='green.400'>Learn More</Link>
-        </Box>
-        <Box width={['100%', '40%']}>
+        </ChakraBox>
+        <ChakraBox transition={{ duration: 0.8 }} initial={{ translateX: -50, opacity: 0 }} whileInView={{ translateX: 0, opacity: 1 }} width={['100%', '40%']}>
           <Image src='/images/collect-1.svg' alt='Collect and Gather all your reviews in one place' />
-        </Box>
+        </ChakraBox>
       </Stack>
 
       <Stack direction={['column', 'row']} px={[8, 20]} py='8' spacing='6' bgColor='white'>
-        <Box width={['100%', '40%']}>
+        <ChakraBox transition={{ duration: 0.8 }} initial={{ translateX: -50, opacity: 0 }} whileInView={{ translateX: 0, opacity: 1 }}  width={['100%', '40%']}>
           <Image src='/images/organize.svg' alt='Manage and Organize reviews' />
-        </Box>
-        <Box width={['100%', '60%']}>
+        </ChakraBox>
+        <ChakraBox transition={{ duration: 0.5 }} initial={{ translateX: -600, opacity: 0 }} whileInView={{ translateX: 0, opacity: 1 }} width={['100%', '60%']}>
           <Heading fontSize='sm' color='orange.500' textTransform='uppercase' fontWeight='medium'>Manage</Heading>
           <Heading lineHeight='1'  color='grey.700' fontSize='5xl' mb='2'>Manage reviews and integrations</Heading>
           <Text color='gray.500'>You don{'\''}t have to open an Excel sheet to gather your feedback. ReviewGush gives you a powerful dashboard to manage all your reviews across all platforms.</Text>
@@ -53,11 +59,11 @@ const Home: NextPage = () => {
             <Checklist as='li'>You can download reviews and export them in different formats</Checklist>
           </UnorderedList>
           <Link href='#' color='green.400'>Learn More</Link>
-        </Box>
+        </ChakraBox>
       </Stack>
 
       <Stack direction={['column', 'row']} px={[8, 20]} py='8' spacing='6' bgColor='white'>
-        <Box width={['100%', '60%']}>
+        <ChakraBox transition={{ duration: 0.5 }} initial={{ translateX: -600, opacity: 0 }} whileInView={{ translateX: 0, opacity: 1 }} width={['100%', '60%']}>
           <Heading fontSize='sm' color='orange.500' textTransform='uppercase' fontWeight='medium'>Share</Heading>
           <Heading lineHeight='1' color='grey.700' fontSize='5xl' mb='2'>Share and Display your reviews</Heading>
           <Text color='gray.500'>Rather than sending WhatsApp screenshots of customers{'\''}/clients{'\''} reviews, share the reviews in a more organized format. With ReviewGush, you can share your collected reviews in a variety of formats, including designs.</Text>
@@ -66,10 +72,10 @@ const Home: NextPage = () => {
             <Checklist as='li'>Curate reviews from all your social media platforms into a wall of love to increase conversions</Checklist>
           </UnorderedList>
           <Link href='#' color='green.400'>Learn More</Link>
-        </Box>
-        <Box width={['100%', '40%']}>
+        </ChakraBox>
+        <ChakraBox transition={{ duration: 0.8 }} initial={{ translateX: -50, opacity: 0 }} whileInView={{ translateX: 0, opacity: 1 }}  width={['100%', '40%']}>
           <Image src='/images/share-online.svg' alt='Share reviews online' />
-        </Box>
+        </ChakraBox>
       </Stack>
 
       <Flex padding={[0, 20]} justify='center'>
