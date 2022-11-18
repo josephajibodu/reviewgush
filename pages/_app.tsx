cda2642cf421a3b8141d96998004f5f28f0f5b2c
cremirdevio <josephajibodu@gmail.com>
2022-11-18 14:57:16 +0100

    React query setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,23 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/Layout'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools'
+
+// Create a client
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ChakraProvider>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  </ChakraProvider>
+  return (
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
