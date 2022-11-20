bc239e2a6a1a6df6ec3213ab18d08fab47b98649
cremirdevio <josephajibodu@gmail.com>
2022-11-20 18:06:09 +0100

    Users can now be successfully added to the mailing list

--- pages/api/get-notified.ts
+++ pages/api/get-notified.ts
@@ -49,8 +49,17 @@ const handler =  async (req: NextApiRequest, res: NextApiResponse) => {
     );
 
     if (response.status >= 400) {
+      const finalRes = await response.json();
+
+      // Check if the user has already been added
+      if (finalRes && finalRes.title == 'Member Exists') {
+        return res.status(200).json({ 
+          message: `${email} has already been added to our waiting list.`
+        });
+      }
+
       return res.status(400).json({
-        error: response.statusText,
+        error: finalRes,
         message: `There was an error subscribing to the newsletter. Hit us up special@reviewgush.com and I'll add you the old fashioned way :(.`,
       });
     }
