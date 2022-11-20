0f675541ec593f6cf862f5cbcfcb5f3aca8e5a11
cremirdevio <josephajibodu@gmail.com>
2022-11-20 11:40:04 +0100

    Email subscription working with react-query

--- components/NewsletterBox.tsx
+++ components/NewsletterBox.tsx
@@ -1,18 +1,79 @@
 import { Box, Button, Flex, Heading, HStack, Input, InputGroup, InputRightElement, StackProps, Text, useToast } from '@chakra-ui/react'
+import { useMutation, useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
 import ThreeStrokes from './BackgroundPatterns/ThreeStrokes'
 
-export default function NewsletterBox({...props}: StackProps) {
+export default function NewsletterBox({ ...props }: StackProps) {
   const toast = useToast();
+  const [emailAddress, setEmailAddress] = useState<string>();
 
-  const subscribeToNewsletter = () => {
-    toast({
-      title: "Email Subscribed",
-      description: "You have suceesfully subscribed to our email list. Be sure to hear from us once the product launches.",
-      status: "success",
-      isClosable: true,
-      duration: 5000,
-      position: 'top'
-    })
+  const handleSubscriptionRequest = async (data: { email: string }) => {
+    const response = await fetch('/api/subscribeUser', {
+      body: JSON.stringify({ email: emailAddress }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    });
+
+    if (response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    return response.json();
+  }
+
+  const { mutateAsync, mutate } = useMutation({
+    mutationFn: handleSubscriptionRequest,
+    onSuccess: () => {
+
+    }
+  });
+
+
+
+  const subscribeToNewsletter = async (e: React.FormEvent<HTMLDivElement>) => {
+    e.preventDefault();
+
+    if (!emailAddress) {
+      return toast({
+        title: "Ooops!",
+        description: "Invalid email address.",
+        status: "error",
+        isClosable: true,
+        duration: 5000,
+        position: 'top'
+      });
+    }
+
+    try {
+      await mutateAsync({ email: emailAddress });
+
+      toast({
+        title: "Email Subscribed",
+        description: "You have suceesfully subscribed to our email list. Be sure to hear from us once the product launches.",
+        status: "success",
+        isClosable: true,
+        duration: 5000,
+        position: 'top'
+      });
+  
+      setEmailAddress("");
+
+    } catch (error) {
+      console.log(error);
+      
+      toast({
+        title: "Email Subscription Error",
+        description: "An unknow error occured",
+        status: "error",
+        isClosable: true,
+        duration: 5000,
+        position: 'top'
+      });
+    }
+
+    
   }
 
   return (
@@ -20,9 +81,9 @@ export default function NewsletterBox({...props}: StackProps) {
       <Heading fontSize='12px' color='white' textTransform='uppercase'>Enjoy 30 days free trial</Heading>
       <Heading fontSize='4xl' color='white'>Sign Up Now</Heading>
       <Text color='white.600'>Be the first to receive the good news from us when we launch. For the first 100 signups, we are giving out 30 days free trial.</Text>
-      <Flex pos='relative' align='center' minWidth='300px' maxWidth='lg' marginTop='6'>
-        <Input placeholder='Enter your email' outline='0' minHeight='12' pr='32' border='2px' borderColor='gray.300' _placeholder={{ color: 'gray.200' }} focusBorderColor='gray.300' />
-        <Button onClick={subscribeToNewsletter} pos='absolute' right='1' bgColor='orange.500' _hover={{ bgColor: 'orange.600' }} zIndex="20">Sign Me Up</Button>
+      <Flex as={'form'} onSubmit={subscribeToNewsletter} pos='relative' align='center' minWidth='300px' maxWidth='lg' marginTop='6'>
+        <Input type={"email"} required value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} placeholder='Enter your email' outline='0' minHeight='12' pr='32' border='2px' borderColor='gray.300' _placeholder={{ color: 'gray.200' }} focusBorderColor='gray.300' />
+        <Button type='submit' pos='absolute' right='1' bgColor='orange.500' _hover={{ bgColor: 'orange.600' }} zIndex="20">Sign Me Up</Button>
       </Flex>
       <ThreeStrokes pos='absolute' transform='rotate(45deg)' bottom='60%' left='85%' />
       <ThreeStrokes pos='absolute' transform='rotate(45deg)' top='60%' right='85%' />
