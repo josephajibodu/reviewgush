37cc811c9205b3d41487d0df572c73a037b5f91a
cremirdevio <josephajibodu@gmail.com>
2022-12-26 19:57:14 +0100

    Head meta data updated

---
+++ config/site.config.js
@@ -0,0 +1,18 @@
+/**
+ * This file contains the configuration used for customising the website.
+ */
+ module.exports = {
+
+  title: "The Rock Star IND",
+
+  datetimeFormat: 'DD-MMM-YYYY',
+
+  app_url: "http://localhost:3000",
+
+  color: {
+    primary: "#dd6b20",
+    secondary: "#38a169"
+  },
+
+  
+}
