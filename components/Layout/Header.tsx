8813c1defd5f19d00183436e67ad90aa408bc4cc
cremirdevio <josephajibodu@gmail.com>
2022-12-26 23:06:25 +0100

    Links added

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -249,21 +249,21 @@ interface NavItem {
 }
 
 const NAV_ITEMS: Array<NavItem> = [
-  // {
-  //   label: 'Inspiration',
-  //   children: [
-  //     {
-  //       label: 'Explore Design Work',
-  //       subLabel: 'Trending Design to inspire you',
-  //       href: '#',
-  //     },
-  //     {
-  //       label: 'New & Noteworthy',
-  //       subLabel: 'Up-and-coming Designers',
-  //       href: '#',
-  //     },
-  //   ],
-  // },
+  {
+    label: 'Inspiration',
+    children: [
+      {
+        label: 'Explore Design Work',
+        subLabel: 'Trending Design to inspire you',
+        href: '#',
+      },
+      {
+        label: 'New & Noteworthy',
+        subLabel: 'Up-and-coming Designers',
+        href: '#',
+      },
+    ],
+  },
   // {
   //   label: 'Find Work',
   //   children: [
