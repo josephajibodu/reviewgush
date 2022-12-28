8173f25d1d7d1cd4c63cf92878be21be5611794d
cremirdevio <josephajibodu@gmail.com>
2022-12-28 13:36:45 +0100

    AuthServiceManager created for Authentication

---
+++ services/AuthManagerService.ts
@@ -0,0 +1,21 @@
+export default class AuthManagerService {
+  static logUserIn() {
+
+  }
+
+  static logUserOut() {
+
+  }
+
+  static registerUser() {
+
+  }
+
+  static getUser() {
+
+  }
+
+  static isLoggedIn() {
+    
+  }
+}
