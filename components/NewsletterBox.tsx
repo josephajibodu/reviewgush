32695e7d3b2ba53fb430d28d273bac0040c91227
cremirdevio <josephajibodu@gmail.com>
2022-12-31 19:52:15 +0100

    Newsletter component updated

--- components/NewsletterBox.tsx
+++ components/NewsletterBox.tsx
@@ -6,6 +6,7 @@ import ThreeStrokes from './BackgroundPatterns/ThreeStrokes'
 export default function NewsletterBox({ ...props }: StackProps) {
   const toast = useToast();
   const [emailAddress, setEmailAddress] = useState<string>();
+  const [fullName, setFullName] = useState<string>();
 
   const handleSubscriptionRequest = async (data: { email: string }) => {
 
@@ -78,10 +79,11 @@ export default function NewsletterBox({ ...props }: StackProps) {
 
   return (
     <Flex overflow='hidden' pos='relative' flexDir='column' align='center' maxWidth='3xl' paddingX='14' paddingY='20' color='white' rounded='2xl' textAlign='center' bgColor='green.500' {...props}>
-      <Heading fontSize='12px' color='white' textTransform='uppercase'>Enjoy 30 days free trial</Heading>
-      <Heading fontSize='4xl' color='white'>Sign Up Now</Heading>
-      <Text color='white.600'>Be the first to receive the good news from us when we launch. For the first 100 signups, we are giving out 30 days free trial.</Text>
+      <Heading fontSize='12px' color='white' textTransform='uppercase'>Enjoy 30 days free pro trial </Heading>
+      <Heading fontSize='4xl' color='white' mb={"4"}>Join Our Waitlist</Heading>
+      <Text color='white.600'>Be the first to receive the good news from us when we launch. For the first 100 signups, we are giving out 30 days free trial for our pro subscriptions.</Text>
       <Flex as={'form'} onSubmit={subscribeToNewsletter} pos='relative' align='center' minWidth='300px' maxWidth='lg' marginTop='6'>
+        <Input type={"text"} required value={fullName} onChange={(e) => setFullName(e.target.value)} placeholder='Full Name' outline='0' minHeight='12' mr={["0", "4 "]} border='2px' borderColor='gray.300' _placeholder={{ color: 'gray.200' }} focusBorderColor='gray.300' />
         <Input type={"email"} required value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} placeholder='Enter your email' outline='0' minHeight='12' pr='32' border='2px' borderColor='gray.300' _placeholder={{ color: 'gray.200' }} focusBorderColor='gray.300' />
         <Button type='submit' pos='absolute' right='1' bgColor='orange.500' _hover={{ bgColor: 'orange.600' }} zIndex="20">
           {isLoading && <CircularProgress isIndeterminate value={100} size='24px' color={"green.300"} m={"4"} />}
